463f7040d0f48f3b04861a6dd0320a137ae56b15
Stujo <stujo@github.com>
2015-10-01 09:26:54 -0700

    Add Contacts and Seed Data
    rails g scaffold Contact name:string phone:string

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Contact.create!([
+  {name: 'Bob', phone: '415-555-5555'},
+  {name: 'Mary', phone: '510-555-5555'},
+  {name: 'Peter', phone: '418-555-5555'},
+  {name: 'Paul', phone: '915-555-5555'},
+  {name: 'Simone', phone: '775-555-5555'},
+  {name: 'Julie', phone: '707-555-5555'},
+  {name: 'Wilson', phone: '551-555-5555'},
+  {name: 'Fran', phone: '814-555-5555'},
+  ])
+
